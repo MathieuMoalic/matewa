73fa74d19c6fb49263a9964fee3e8a22abe8a91e
MathieuMoalic <ripirpk@gmail.com>
2020-09-25 14:42:15 +0200

    added models to admin page and setup the database

--- matewa/blog/views.py
+++ matewa/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'Me',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'wrz 25'
-    },
-    {
-        'author':'you',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'wrz 26'
-    }    
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
