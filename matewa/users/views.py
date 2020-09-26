1c76eec02cf8c1315a1e9171d238eed490649ad1
MathieuMoalic <ripirpk@gmail.com>
2020-09-26 12:36:27 +0200

    p11, added pagination, but user list header doesn't work

--- matewa/users/views.py
+++ matewa/users/views.py
@@ -7,9 +7,9 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created! You may log in')
+            form.save()
             return redirect('blog-home')
     else:
         form = UserRegisterForm()
