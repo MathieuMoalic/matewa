168f2054fd8a59e2f846d95e9d5a8e3d545b8d55
MathieuMoalic <ripirpk@gmail.com>
2020-09-27 17:00:50 +0200

    updated setting

--- matewa/settings.py
+++ matewa/settings.py
@@ -21,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's)iowhz@^p^u9it9ga5d$eegjxn-c-*ucou=e0jl8cf4#qmvkr'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ['18.196.183.93/']
+ALLOWED_HOSTS = ['127.0.0.1','matewa.herokuapp.com/']
 
 # Application definition
 
